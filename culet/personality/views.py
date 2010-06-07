007cf0f05acd6c89357df24b3560039c5d127340
Calvin Spealman <ironfroggy@gmail.com>
2010-06-06 20:50:44 -0400

    Added possible view decorators to use

--- culet/personality/views.py
+++ culet/personality/views.py
@@ -1,10 +1,57 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+
+from culet.personality.models import Personality
+
+class SuspicionError(Exception):
+    def __init__(self, msg, user):
+        self.msg = msg
+        self.user = user
+        self.args = (msg, user)
+
+def _viewier_dec(f, template):
+    def _(request, *args, **kwargs):
+        try:
+            data = f(request, *args, **kwargs)
+            return render_to_response(
+                template,
+                RequestContext(request, data)
+            )
+        except SuspicionError:
+            return render_to_response("culet/error.html")
+    return _
+def viewier(template):
+    return lambda func:_viewier_dec(func, template)
 
 def myselves(request):
     """List of current user's personalities."""
 
+@viewier("culet/become.html")
 def become(request, alternate):
     """Log a user in as one of their alternates."""
+
+    current_user = request.user
+    try:
+        master_user = current_user.master_user
+    except AttributeError:
+        master_user = current_user
+
+    try:
+        personality = master_user.personalities.filter(username=alternate)
+    except User.DoesNotExist:
+        if User.objects.filter(username=alternate).count():
+            raise SuspicionError("User tried to become a personality they do not own", user=current_user)
+        else:
+            raise SuspicionError("User tried to become a personality that does not exist", user=current_user)
+
+    else:
+        login(request, personality)
+
+        return {
+            'previous_user': current_user,
+            'became': personality,
+            'master_user': master_user,
+        }
 
 def delete(request, alternate, confirmation=None):
     """Delete a personality."""
