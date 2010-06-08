561b51268ac1706635311eb05b61cf65489354ba
Calvin Spealman <ironfroggy@gmail.com>
2010-06-08 11:36:26 -0400

    Implement a culet.personality middleware that replaces User with Personality in request.user

--- culet/personality/views.py
+++ culet/personality/views.py
@@ -2,8 +2,12 @@ from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from culet.personality.models import Personality
+from culet.personality.forms import PersonalityForm
 
 class SuspicionError(Exception):
     def __init__(self, msg, user):
@@ -13,10 +17,27 @@ class SuspicionError(Exception):
 class SuspicionError_Permission(SuspicionError): pass
 class SuspicionError_Invalid(SuspicionError): pass
 
-def _viewier_dec(f, template):
+def _viewier_dec(f, template, get_form=None, success_url=None):
     def _(request, *args, **kwargs):
         try:
-            data = f(request, *args, **kwargs)
+            if request.method == 'GET' and get_form is not None:
+                data = {
+                    'form': get_form(request)
+                }
+            elif request.method == 'POST' and get_form is not None:
+                form = get_form(request, request.POST)
+                if form.is_valid():
+                    form.save()
+                    if success_url is not None:
+                        return HttpResponseRedirect(reverse(success_url))
+                else:
+                    data = {
+                        'form': form,
+                    }
+            else:
+                data = f(request, *args, **kwargs)
+                if success_url is not None:
+                    return HttpResponseRedirect(reverse(success_url))
             return render_to_response(
                 template,
                 RequestContext(request, data)
@@ -25,8 +46,8 @@ def _viewier_dec(f, template):
             return render_to_response("culet/error.html", RequestContext(request, {}))
     _.wrapped = f
     return _
-def viewier(template):
-    return lambda func:_viewier_dec(func, template)
+def viewier(template, **kwargs):
+    return lambda func:_viewier_dec(func, template, **kwargs)
 
 @viewier('culet/myselves.html')
 def myselves(request):
@@ -46,7 +67,7 @@ def myselves(request):
         'personalities': personalities,
     }
 
-@viewier("culet/become.html")
+@viewier("culet/become.html", success_url="personality-myselves")
 def become(request, alternate):
     """Log a user in as one of their alternates."""
 
@@ -60,26 +81,38 @@ def become(request, alternate):
 
     try:
         personality = master_user.personalities.get(username=alternate)
+    except Personality.DoesNotExist:
+        personality = User.objects.get(username=alternate)
+        if personality.personalities.count():
+            pass
+        else:
+            raise
     except User.DoesNotExist:
         if User.objects.filter(username=alternate).count():
             raise SuspicionError_Permission("User tried to become a personality they do not own", user=current_user)
         else:
             raise SuspicionError_Invalid("User tried to become a personality that does not exist", user=current_user)
 
-    else:
-        login(request, personality)
+    personality.backend = settings.AUTHENTICATION_BACKENDS[0]
+    login(request, personality)
 
-        return {
-            'previous_user': current_user,
-            'became': personality,
-            'master_user': master_user,
-        }
+    return {
+        'previous_user': current_user,
+        'became': personality,
+        'master_user': master_user,
+    }
 
 def delete(request, alternate, confirmation=None):
     """Delete a personality."""
 
+@viewier('culet/create.html',
+    get_form=PersonalityForm,
+    success_url='personality-myselves',
+    )
 def create(request):
     """Create a personality."""
+
+    pass
 
 def update(request, alternate):
     """Update a personality."""
--- testproject/settings.py
+++ testproject/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+    'culet.personality.middleware.PersonalityMiddleware',
 )
 
 ROOT_URLCONF = 'testproject.urls'
