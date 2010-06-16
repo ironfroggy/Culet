4202afbb9c2743b0e2e0ff415970d089d72d6c89
Calvin Spealman <ironfroggy@gmail.com>
2010-06-16 09:36:22 -0400

    Add list of current types to pick from when creating a post."

--- culet/anycontent/urls.py
+++ culet/anycontent/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns("culet.anycontent.views",
     url(r'^$', 'list', name='anycontent-list'),
+    url(r'^post/$', 'post', name='anycontent-post-types'),
     url(r'^post/(\d+)/$', 'post', name='anycontent-post'),
 )
--- culet/anycontent/views.py
+++ culet/anycontent/views.py
@@ -1,7 +1,8 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.contenttypes.models import ContentType
 
-from culet.anycontent.models import Header
+from culet.anycontent.models import Header, _types
 from culet.anycontent.forms import AnyContentForm
 
 def list(request):
@@ -12,10 +13,21 @@ def list(request):
         "anycontent/list.html",
         RequestContext(request, {"posts": all}))
 
-def post(request, content_type):
+def post(request, content_type=None):
 
-    forms = AnyContentForm(request, content_type) 
+    if content_type is None:
+        forms = []
+    else:
+        forms = AnyContentForm(request, content_type) 
+
+    posttypes = [
+        {
+            'name': t['name'],
+            'content_type_id': ContentType.objects.get_for_model(t['model']).id,
+        }
+        for t in _types.values()
+    ]
 
     return render_to_response(
         "anycontent/form.html",
-        RequestContext(request, {"forms": forms}))
+        RequestContext(request, {"forms": forms, "posttypes": posttypes}))
