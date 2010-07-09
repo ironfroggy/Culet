733c572300861fa45f2e4ee15529f00e750177cf
Calvin Spealman <ironfroggy@gmail.com>
2010-07-09 11:07:50 -0400

    Slug is hidden an auto-generated.

--- culet/anycontent/forms.py
+++ culet/anycontent/forms.py
@@ -1,6 +1,7 @@
 from sys import stderr
 from django import forms
 from django.contrib.contenttypes.models import ContentType
+from django.template.defaultfilters import slugify
 
 from culet.anycontent.models import Header, content_classes
 
@@ -14,11 +15,12 @@ class _ccc(object):
                 cc['name'],
             )
 
-HF_DEBUG_FIELDS = None
+HF_DEBUG_FIELDS = forms.HiddenInput
 
 class HeaderForm(forms.ModelForm):
     content_type_name = forms.ChoiceField(widget=HF_DEBUG_FIELDS, choices=_ccc())
     content_id = forms.CharField(widget=HF_DEBUG_FIELDS)
+    slug = forms.CharField(widget=HF_DEBUG_FIELDS)
     class Meta:
         model = Header
 
@@ -31,6 +33,7 @@ def AnyContentForm(request, content_type_name):
         if  cf.is_valid():
             content = cf.save()
             hf.data = dict((k, v) for (k, v) in hf.data.items())
+            hf.data[hf.add_prefix('slug')] = slugify(hf.data[hf.add_prefix('title')])
             hf.data[hf.add_prefix('content')] = content.id
             hf.data[hf.add_prefix('content_type_name')] = content_classes[content_type_name]['name']
             hf.data[hf.add_prefix('content_id')] = content.id
