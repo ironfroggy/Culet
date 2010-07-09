be2670c4b69c2ccbc44010e70f9c5e90db9dbac6
Calvin Spealman <ironfroggy@gmail.com>
2010-07-08 21:26:57 -0400

    Making anycontent.Header.slug unique

---
+++ culet/anycontent/migrations/0003_auto__add_unique_header_slug.py
@@ -0,0 +1,47 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding unique constraint on 'Header', fields ['slug']
+        db.create_unique('anycontent_header', ['slug'])
+
+
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'Header', fields ['slug']
+        db.delete_unique('anycontent_header', ['slug'])
+
+
+    models = {
+        'anycontent.header': {
+            'Meta': {'object_name': 'Header'},
+            'content_id': ('django.db.models.fields.IntegerField', [], {}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'content_type_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'edited': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'anycontent.plaintext': {
+            'Meta': {'object_name': 'PlainText'},
+            'body': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['anycontent']
